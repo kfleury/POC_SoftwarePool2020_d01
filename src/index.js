74ad87fd5e6bcf1c1a211df53db7902bc98aca09
kfleury <killian.fleury@epitech.eu>
2020-03-02 17:58:53 +0100

    [add]with .env

--- src/index.js
+++ src/index.js
@@ -8,28 +8,27 @@ const bodyParser = require('body-parser');
 
 app.use(cookieParser());
 app.use(bodyParser.text());
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 
 function startServer() {
   app.get('/hello', (req, res) => {
     res.send('world');
   });
   app.get('/repeat-my-fixed', (req, res) => {
-    const better = 'For better and for worst\n';
-    res.send(better);
+    res.send(process.env.FIXED_MESSAGE);
     req.status = 200;
   });
   app.get('/repeat-my-query', (req, res) => {
     const { message } = req.query;
     if (message.length === 0) {
-      req.status = (400);
+      req.status = process.env.ERROR_STATUS;
       res.send('Bad Request\n');
     } else res.send(message);
   });
   app.post('/repeat-my-body', (req, res) => {
     const str = req.body;
     if (Object.values(str).length === 0) {
-      req.status = 400;
+      req.status = process.env.ERROR_STATUS;
       res.send('Bad Request\n');
     } else res.send(str);
   });
@@ -39,7 +38,7 @@ function startServer() {
     if ((Object.keys(str)[position] === 'x-message') && Object.values(str)[position].length !== 0) {
       res.send(Object.values(str)[1]);
     } else {
-      req.status = 400;
+      req.status = process.env.ERROR_STATUS;
       res.send('Bad Request\n');
     }
   });
@@ -49,14 +48,14 @@ function startServer() {
     if ((Object.keys(str)[position] === 'message') && Object.values(str)[position].length !== 0) {
       res.send(Object.values(str)[1]);
     } else {
-      req.status = 400;
+      req.status = process.env.ERROR_STATUS;
       res.send('Bad Request\n');
     }
   });
   app.get('/repeat-my-param/:message', (req, res) => {
     res.send(req.params.message);
   });
-  app.listen(8080);
+  app.listen(process.env.SERVER_PORT);
 }
 
 startServer();
